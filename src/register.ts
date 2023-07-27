eb865b1337d164a92059af96fe4092b3e007a2c0
inaniwaudon <w.yuto0916@gmail.com>
2023-07-27 22:56:56 +0900

    initial commit

---
+++ src/register.ts
@@ -0,0 +1,39 @@
+import { KIRO_COMMAND } from './commands.js';
+import fetch from 'node-fetch';
+
+const token = process.env.DISCORD_TOKEN;
+const applicationId = process.env.DISCORD_APPLICATION_ID;
+
+if (!token) {
+	throw new Error('DISCORD_TOKEN is undefined.');
+}
+if (!applicationId) {
+	throw new Error('DISCORD_APPLICATION_ID is undefined.');
+}
+
+const registerCommands = async (url: string) => {
+	const response = await fetch(url, {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bot ${token}`,
+		},
+		method: 'PUT',
+		body: JSON.stringify([KIRO_COMMAND]),
+	});
+
+	if (response.ok) {
+		console.log('Registered all commands');
+	} else {
+		console.error('Error registering commands');
+		const text = await response.text();
+		console.error(text);
+	}
+	return response;
+};
+
+const registerGlobalCommands = async () => {
+	const url = `https://discord.com/api/v10/applications/${applicationId}/commands`;
+	await registerCommands(url);
+};
+
+await registerGlobalCommands();
